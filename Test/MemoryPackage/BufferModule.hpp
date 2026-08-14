d0073ce5c6a489fe49e636941a02a47403b5b863
Wither__ <hhex16@gmail.com>
2026-08-14 12:11:52 +0200

    Test: added Memory::Buffer::copy() test

--- Test/MemoryPackage/BufferModule.hpp
+++ Test/MemoryPackage/BufferModule.hpp
@@ -5,8 +5,6 @@
 
 #include <Memory.hpp>
 
-#include <string.h>
-
 namespace Melon::Test
 {
         TARWI_MODULE(BufferModule)
@@ -16,14 +14,28 @@ namespace Melon::Test
                 TARWI_UNIT(unitCheckInit)
                 {
                         char data[] = "abcdefg";
-                        Memory::Buffer<char> buf(data, strlen(data));
+                        Memory::Buffer<char> buf(data, Memory::C::strlen(data));
 
-                        TARWI_EXPECT(strcmp(buf.get(), data) == 0);
+                        TARWI_EXPECT(Memory::C::strcmp(buf.get(), data) == 0);
+                }
+
+                TARWI_UNIT(unitCheckCopy)
+                {
+                        char a_data[] = "01234";
+                        char b_data[] = "abcde";
+
+                        Memory::Buffer<char> a(a_data, Memory::C::strlen(a_data));
+                        Memory::Buffer<char> b(b_data, Memory::C::strlen(b_data));
+
+                        a.copy(b, Memory::C::strlen(a.get()));
+
+                        TARWI_EXPECT(Memory::C::strcmp(a.get(), b.get()) == 0);
                 }
 
                 TARWI_MODULE_MAIN()
                 {
                         TARWI_CALL_UNIT(unitCheckInit);
+                        TARWI_CALL_UNIT(unitCheckCopy);
                 }
         };
 } // namespace Melon::Test
