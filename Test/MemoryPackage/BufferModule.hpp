b292952acd434db78b82245c0f2d936bbb855ce2
Wither__ <hhex16@gmail.com>
2026-08-17 12:19:28 +0200

    Test: overflow on buffer copy

--- Test/MemoryPackage/BufferModule.hpp
+++ Test/MemoryPackage/BufferModule.hpp
@@ -4,6 +4,7 @@
 #include <TarwiGlobals.hpp>
 
 #include <Memory.hpp>
+#include <Exceptions.hpp>
 
 namespace Melon::Test
 {
@@ -32,10 +33,28 @@ namespace Melon::Test
                         TARWI_EXPECT(strcmp(a.get(), b.get()) == 0);
                 }
 
+                TARWI_UNIT(unitCopyOverflow)
+                {
+                        char a_data[] = "01234";
+                        char b_data[] = "abcde";
+
+                        Memory::Buffer<char> a(a_data, strlen(a_data));
+                        Memory::Buffer<char> b(b_data, strlen(b_data));
+
+                        try {
+                                a.copy(b, strlen(a.get()) + 2);
+                        } catch (const Exceptions::BufferOverflow &e) {
+                                TARWI_OUTPUT("buffer copy failed: %s\n", e.what());
+                        }
+
+                        TARWI_EXPECT(strcmp(a.get(), b.get()) != 0);
+                }
+
                 TARWI_MODULE_MAIN()
                 {
                         TARWI_CALL_UNIT(unitCheckInit);
                         TARWI_CALL_UNIT(unitCheckCopy);
+                        TARWI_CALL_UNIT(unitCopyOverflow);
                 }
         };
 } // namespace Melon::Test
